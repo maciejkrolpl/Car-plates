864185c1b79b9d66b3d321482d33c2513e362bdc
Maciej Król <Maciej.Krol@email.fake>
2023-09-28 15:16:12 +0200

    Various search types

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let searchType = "strict";
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
@@ -5,22 +7,57 @@ document.addEventListener(
     input.addEventListener("input", (e) => {
       search(e.target.value.toUpperCase());
     });
+
+    const buttons = document.getElementsByClassName("slds-button");
+    [...buttons].forEach((button) => {
+      button.addEventListener("click", (e) => {
+        clickButton(e.target.id);
+      });
+    });
   },
   false
 );
 
+function clickButton(clickedId) {
+  const buttons = document.getElementsByClassName("slds-button");
+  [...buttons].forEach((button) => {
+    const currentId = button.id;
+    if (clickedId === currentId) {
+      searchType = clickedId;
+      button.classList.add("slds-button_brand");
+      button.classList.remove("slds-button_neutral");
+    } else {
+      button.classList.remove("slds-button_brand");
+      button.classList.add("slds-button_neutral");
+    }
+  });
+}
+
 function search(typed) {
-  let result = [];
+  if (!typed?.length) {
+    const output = document.getElementById("output");
+    output.innerHTML = "";
+    return;
+  }
+
+  const filterToSearchType = {
+    strict: (entry) => entry.plate === typed,
+    nonstrict: (entry) => entry.plate.startsWith(typed),
+    descr: (entry) => entry.district.toUpperCase().includes(typed),
+  };
+
+  const result = [];
+  const filter = filterToSearchType[searchType];
+
   [polish, ukrainian, german].forEach((country) => {
-    const matched = country.entries
-      .filter((entry) => entry.plate === typed)
-      .map((plate) => {
-        return `<div class="slds-card__body slds-card__body_inner"><b>${plate.plate}</b> - ${plate.district}</div>`
-      });
+    const matched = country.entries.filter(filter).map((plate) => {
+      return `<div class="slds-card__body slds-card__body_inner"><b>${plate.plate}</b> - ${plate.district}</div>`;
+    });
 
     if (matched.length) {
       result.push(
-        `<article class="slds-card">
+        `
+<article class="slds-card">
   <div class="slds-card__header slds-grid">
     <header class="slds-media slds-media_center slds-has-flexi-truncate">
       <div class="slds-media__figure">
